e7be47f477ccd6c4a48ca63b7706bae2818dfba1
Jean-benoit <jibusss50@gmail.com>
2022-05-30 21:06:42 +0200

    Refactoring #3

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 module.exports = {
   extends: ['./rules/templates', './rules/typescript'].map(require.resolve),
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
   rules: {},
 };
