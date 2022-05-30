77543da6180b52d471937cb07f69074de501f1f4
Jean-benoit <jibusss50@gmail.com>
2022-05-30 19:22:51 +0200

    Refactoring

---
+++ index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  extends: ['./templates', './typescript'].map(require.resolve),
+};
---
+++ templates.js
@@ -0,0 +1,4 @@
+module.exports = {
+  extends: ['plugin:@angular-eslint/template/recommended'],
+  files: ['*.html'],
+};
