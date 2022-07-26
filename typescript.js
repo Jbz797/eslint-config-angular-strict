8bc790d2a9ba196b69e6d77873bf4cfb8416d1ed
Jean-benoit <jibusss50@gmail.com>
2022-07-26 17:30:37 +0200

    Add "@angular-eslint/no-pipe-impure" rule

--- typescript.js
+++ typescript.js
@@ -29,6 +29,7 @@ module.exports = {
     '@angular-eslint/no-output-on-prefix': ['error'],
     '@angular-eslint/no-output-rename': ['error'],
     '@angular-eslint/no-outputs-metadata-property': ['error'],
+    '@angular-eslint/no-pipe-impure': ['error'],
 
     '@typescript-eslint/lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
     '@typescript-eslint/no-explicit-any': 'off',
