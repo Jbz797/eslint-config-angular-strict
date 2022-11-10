e58fab74d8c55d6a5a2bfa0cb42780304968eaae
Jean-benoit <jibusss50@gmail.com>
2022-11-10 15:27:08 +0100

    Add "template/accessibility-interactive-supports-focus" rule

--- template.js
+++ template.js
@@ -2,5 +2,6 @@ module.exports = {
   extends: ['plugin:@angular-eslint/template/recommended'],
   rules: {
     '@angular-eslint/template/accessibility-elements-content': ['error'],
+    '@angular-eslint/template/accessibility-interactive-supports-focus': ['error'],
   },
 };
