e3954a7c881ccae5d337debac2b9e06ae27478b5
Jean-benoit <jibusss50@gmail.com>
2022-11-15 15:36:40 +0100

    Add "@angular-eslint/template/accessibility-role-has-required-aria" role

--- template.js
+++ template.js
@@ -3,5 +3,6 @@ module.exports = {
   rules: {
     '@angular-eslint/template/accessibility-elements-content': 'error',
     '@angular-eslint/template/accessibility-label-has-associated-control': 'error',
+    '@angular-eslint/template/accessibility-role-has-required-aria': 'error',
   },
 };
