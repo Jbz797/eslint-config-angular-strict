5a4b003bb11e47199ebb12481e6f92023be6c818
Jean-benoit <jibusss50@gmail.com>
2022-11-17 14:51:38 +0100

    Add "@angular-eslint/template/accessibility-table-scope" rule

--- template.js
+++ template.js
@@ -2,7 +2,9 @@ module.exports = {
   extends: ['plugin:@angular-eslint/template/recommended'],
   rules: {
     '@angular-eslint/template/accessibility-elements-content': 'error',
+    '@angular-eslint/template/accessibility-interactive-supports-focus': 'error',
     '@angular-eslint/template/accessibility-label-has-associated-control': 'error',
     '@angular-eslint/template/accessibility-role-has-required-aria': 'error',
+    '@angular-eslint/template/accessibility-table-scope': 'error',
   },
 };
