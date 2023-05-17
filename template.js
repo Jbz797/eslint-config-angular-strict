b056ab230bb52a743675587143f6d332b13f7d9f
Jean-benoit <jibusss50@gmail.com>
2023-05-17 10:36:28 +0200

    Fix rules had a prefix of "accessibility-" in their name

--- template.js
+++ template.js
@@ -1,21 +1,21 @@
 module.exports = {
   extends: ['plugin:@angular-eslint/template/recommended'],
   rules: {
-    '@angular-eslint/template/accessibility-elements-content': 'error',
-    '@angular-eslint/template/accessibility-label-has-associated-control': 'error',
-    '@angular-eslint/template/accessibility-role-has-required-aria': 'error',
-    '@angular-eslint/template/accessibility-table-scope': 'error',
-    '@angular-eslint/template/accessibility-valid-aria': 'error',
     '@angular-eslint/template/attributes-order': ['error', { alphabetical: true }],
     '@angular-eslint/template/banana-in-box': 'error',
     '@angular-eslint/template/conditional-complexity': 'error',
     '@angular-eslint/template/cyclomatic-complexity': ['error', { maxComplexity: 10 }],
+    '@angular-eslint/template/elements-content': 'error',
     '@angular-eslint/template/eqeqeq': 'error',
+    '@angular-eslint/template/label-has-associated-control': 'error',
     '@angular-eslint/template/mouse-events-have-key-events': 'error',
     '@angular-eslint/template/no-any': 'error',
     '@angular-eslint/template/no-autofocus': 'error',
     '@angular-eslint/template/no-call-expression': 'error',
     '@angular-eslint/template/no-distracting-elements': 'error',
-    '@angular-eslint/template/no-duplicate-attributes': 'error'
+    '@angular-eslint/template/no-duplicate-attributes': 'error',
+    '@angular-eslint/template/role-has-required-aria': 'error',
+    '@angular-eslint/template/table-scope': 'error',
+    '@angular-eslint/template/valid-aria': 'error',
   },
 };
