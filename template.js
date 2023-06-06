2796fd2dc49424df89713b0cf740148db7dda718
Jean-benoit <jibusss50@gmail.com>
2023-06-06 15:14:39 +0200

    Add "@angular-eslint/template/no-negated-async" rule

--- template.js
+++ template.js
@@ -15,6 +15,7 @@ module.exports = {
     '@angular-eslint/template/no-distracting-elements': 'error',
     '@angular-eslint/template/no-duplicate-attributes': 'error',
     '@angular-eslint/template/no-interpolation-in-attributes': 'error',
+    '@angular-eslint/template/no-negated-async': 'error',
     '@angular-eslint/template/role-has-required-aria': 'error',
     '@angular-eslint/template/table-scope': 'error',
     '@angular-eslint/template/valid-aria': 'error',
