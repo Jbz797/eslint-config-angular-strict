378cd6b4264bf40f6f237aea2ba32a999017ffcc
Jean-benoit <jibusss50@gmail.com>
2023-07-19 18:17:42 +0200

    Add "'@angular-eslint/template/use-track-by-function'" rule

--- template.js
+++ template.js
@@ -20,6 +20,7 @@ module.exports = {
     '@angular-eslint/template/role-has-required-aria': 'error',
     '@angular-eslint/template/table-scope': 'error',
     '@angular-eslint/template/table-scope': 'error',
+    '@angular-eslint/template/use-track-by-function': 'error',
     '@angular-eslint/template/valid-aria': 'error',
   },
 };
