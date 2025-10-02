4afccd9b35795ffa1028113dd2d8510df6797f48
Jean-benoit <jibusss50@gmail.com>
2025-10-02 16:41:03 +0200

    Change "@typescript-eslint/consistent-type-imports" to "no-type-imports"

--- index.js
+++ index.js
@@ -64,6 +64,7 @@ export default [
       '@angular-eslint/use-pipe-transform-interface': 'error',
 
       // TypeScript ESLint rules
+      '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'no-type-imports' }],
       '@typescript-eslint/member-ordering': 'error',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-extraneous-class': ['error', { allowEmpty: true, allowStaticOnly: true }],
