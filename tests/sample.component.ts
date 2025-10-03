4043337df79eda250e1b5f9d3d98ba2d9561868a
Jean-benoit <jibusss50@gmail.com>
2025-10-03 11:23:59 +0200

    Remove "prefer-destructuring" rule

--- tests/sample.component.ts
+++ tests/sample.component.ts
@@ -1,34 +1,21 @@
-import { Component, OnInit, Input } from '@angular/core';
-
-// Test component with intentional violations to validate ESLint rules
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-sample',
   templateUrl: './sample.component.html',
-  styleUrls: ['./sample.component.css'] // Should trigger consistent-component-styles
 })
-export class SampleComponent implements OnInit {
-  @Input() config: any; // Should trigger no-explicit-any warning
+
+export class SampleComponent {
+  @Input() config: any;
   private _items!: string[]; // Non-null assertion
   public name = 'sample';
 
-  constructor() {} // Empty constructor should trigger warning
-
-  ngOnInit(): void {
-    this.loadData();
-  }
-
-  loadData() {
-    this._items = this.config?.items || [];
-  }
-
-  // Method should be after getter based on member-ordering
-  processItems() {
-    return this._items.map(item => item.toUpperCase()); // Missing arrow function parens
-  }
-
-  // Getter should be before methods
-  get itemCount(): number {
-    return this._items?.length || 0;
+  constructor() {
+    const obj1 = {
+      a: 1,
+      C: 4,
+      b: 2,
+      c: 3,
+    };
   }
 }
