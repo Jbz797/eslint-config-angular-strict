e2ef9e1ca10a75bcb0a914ec8468e3394fdd0957
Jean-benoit <jibusss50@gmail.com>
2025-10-18 10:53:12 +0200

    Updates dependencies

--- tests/sample.component.ts
+++ tests/sample.component.ts
@@ -1,25 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sample',
   templateUrl: './sample.component.html',
 })
 
-export class SampleComponent {
-  @Input() config: any;
-  private _items!: string[]; // Non-null assertion
-  public name = 'sample';
-
-  constructor(
-    private _testt = 'test', // 'no-underscore-dangle'
-  ) {
-    const _test = parseInt('42', 7); // 'as-needed' radix
-  }
-
-  private _test() {
-    let test = 0;
-    test++;
-    this._items = ['one', 'two', 'three'];
-    const { qux } = object;
-  }
-}
+export class SampleComponent {}
