03865f578b28b41a4a7dae6e0bb53ba6059251c7
Jean-benoit <jibusss50@gmail.com>
2026-02-09 10:48:51 +0100

    Updates `@stylistic/eslint-plugin` to the latest version

--- tests/eslint.config.js
+++ tests/eslint.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable import-x/extensions, import-x/no-relative-packages, */
+/* eslint-disable import-x/no-useless-path-segments */
 import angularStrict from '../index.js';
 
 export default [...angularStrict];
