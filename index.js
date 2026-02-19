93a321059ae5d6fd12cfff872af09f488986920f
Jean-benoit <jibusss50@gmail.com>
2026-02-19 16:28:16 +0100

    Disable `no-confusing-arrow` rule

--- index.js
+++ index.js
@@ -106,12 +106,8 @@ export default [
       '@stylistic/lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
       '@stylistic/max-len': ['error', { code: 165, tabWidth: 2 }],
       '@stylistic/newline-per-chained-call': ['error', { ignoreChainWithDepth: 5 }],
-      '@stylistic/no-extra-parens': ['error', 'all', {
-        ignoredNodes: ['ConditionalExpression'],
-        nestedBinaryExpressions: false,
-        returnAssign: false,
-        ternaryOperandBinaryExpressions: false,
-      }],
+      '@stylistic/no-confusing-arrow': 'off',
+      '@stylistic/no-extra-parens': ['error', 'all', { nestedBinaryExpressions: false, returnAssign: false, ternaryOperandBinaryExpressions: false }],
       '@stylistic/object-curly-newline': [
         'error',
         {
