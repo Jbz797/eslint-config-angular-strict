cadd8e0f3b5542939acda4473df61122c16b9a45
Jean-benoit <jibusss50@gmail.com>
2026-06-09 22:10:22 +0200

    Refine TypeScript and Angular compiler configurations

--- tests/components/sample.component.ts
+++ tests/components/sample.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-sample',
-  templateUrl: './sample.component.html',
-})
+@Component({ selector: 'app-sample', templateUrl: './sample.component.html' })
 
 export class SampleComponent {}
